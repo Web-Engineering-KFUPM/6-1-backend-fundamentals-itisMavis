5f3240693e5e99bc79e0472bee1ee9ac2406e739
itisMavis <maiss.a.khalaf704@gmail.com>
2026-03-30 13:14:56 +0300

    TODO 6 AND 7

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,6 +1,6 @@
 // TODO 1: Import Express
 import express from 'express';
-
+import cors from 'cors';
 
 // TODO 2: Create the Express app and store it in a variable named app
 const app = express();
@@ -17,11 +17,18 @@ app.get('/', (req, res) => {
 
 
 // TODO 6: Create the "/about" route
-
+app.get('/about', (req, res) => {
+  res.send('This is the about route');
+});
 
 
 // TODO 7: Create the "/student" route
-
+app.get('/student', (req, res) => {
+  res.json({
+    name: 'Aisha',
+    major: 'Computer Science'
+  });
+});
 
 
 // TODO 4: Start the server on port 3000
